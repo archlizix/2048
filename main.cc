9e0216bb054d8ea92e91c594c1462fde246bf208
archlizix <archlizix@qq.com>
2021-08-03 22:34:52 +0800

    绘制游戏界面

--- main.cc
+++ main.cc
@@ -7,6 +7,77 @@
 
 using namespace std;
 
+// 格子数
+#define N 4
+// 每个格子的字符长度
+#define WIDTH 5
+
+class Game2048
+{
+public:
+	Game2048()
+	{
+		setTestData();
+	}
+	// 绘制游戏界面
+	void draw()
+	{
+		// 清理屏幕
+		clear();
+		// 居中偏移
+		const int offset = 12;
+		for (int i = 0; i <= N; ++i) {
+			for (int j = 0; j <= N; ++j) {
+				// 相交点
+				drawItem(i * 2, 1 + j * WIDTH + offset, '+');
+				// 竖线
+				if (i != N) {
+					drawItem(i * 2 + 1, 1 + j * WIDTH + offset, '|');
+				}
+				// 横线
+				for (int k = 1; j != N && k < WIDTH; ++k) {
+					drawItem(i * 2, 1 + j * WIDTH + k + offset, '-');
+				}
+				// 每个格子里的数
+				if (i != N && j != N) {
+					drawNum(i * 2 + 1, (j + 1) * WIDTH + offset, data[i][j]);
+				}
+			}
+		}
+		// 提示文字
+		mvprintw(2 * N + 2, (5 * (N - 4) - 1) / 2, "W(UP),S(DOWN),A(LEFT),D(RIGHT),R(RESTART),Q(QUIT)");
+		mvprintw(2 * N + 3, 12 + 5 * (N - 4) / 2, "archlizix");
+	}
+
+	// 方便调试, 随时设置数据
+	void setTestData()
+	{
+		for (int i = 0; i < N; ++i) {
+			for (int j = 0; j < N; ++j) {
+				data[i][j] = 16 << i << j;
+			}
+		}
+	}
+private:
+	int data[N][N];
+
+	// 左上角为（0，0），在指定的位置画一个字符
+	void drawItem(int row, int col, char c)
+	{
+		move(row, col);         addch(c);
+	}
+
+	// 游戏里的数字是右对齐，row, col是数字最后一位所在的位置
+	void drawNum(int row, int col, int num)
+	{
+		while (num > 0) {
+			drawItem(row, col, num % 10 + '0');
+			num /= 10;
+			--col;
+		}
+	}
+};
+
 void initialize()
 {
 	// ncurses初始化
@@ -31,11 +102,11 @@ int main()
 {
 	initialize();
 
+	Game2048 game;
 	char ch = 'n';
+
 	do {
-		move(5, 5);
-		addch(ch);
-		mvprintw(2, 2, "https://github.com/archlizix");
+		game.draw();
 		ch = getch();
 	} while (ch != 'Q' && ch != 'q');
 
